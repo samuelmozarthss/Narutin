27d63d944bcedeef4495a5cebed293eca45f7cf8
Samuel Mozarth <contatosamuelm@gmail.com>
2022-01-11 18:34:18 -0300

    Implementation: Room

--- app/src/main/java/com/example/narutin/ui/MainActivity.kt
+++ app/src/main/java/com/example/narutin/ui/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.narutin.*
+import com.example.narutin.repository.CharacterDAO
+import com.example.narutin.repository.local.AppDatabase
+import com.example.narutin.repository.local.CharacterRepository
+import com.example.narutin.repository.local.CharacterRepositoryImpl
 import com.example.narutin.service.CharacterServices
 import com.example.narutin.service.RetrofitClient
 import com.example.narutin.ui.adapter.CharacterAdapter
@@ -17,6 +21,8 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var mRepository: CharacterRepository
+    private lateinit var mDatabase: AppDatabase
     private lateinit var mRecyclerview: RecyclerView
     private var mCharacterList: ArrayList<CharactersModel> = ArrayList()
     private val mAdapter: CharacterAdapter = CharacterAdapter(mCharacterList)
@@ -28,43 +34,54 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
+        mDatabase = AppDatabase.getDatabase(this)
+
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mRepository = CharacterRepositoryImpl(mDatabase.characterDAO())
 
         val remote = RetrofitClient.createService(CharacterServices::class.java)
         val call: Call<List<CharactersModel>> = remote.list()
 
-        val response = call.enqueue(object : Callback<List<CharactersModel>> {
-
-            override fun onResponse(
-                call: Call<List<CharactersModel>>,
-                response: Response<List<CharactersModel>>
-            ) {
-//                response.body()?.forEach {
-//                    mCharacterList.add(it)
-//                }
-
-                response.body()?.let { charactersList ->
-                    mCharacterList.addAll(charactersList)
-                }
-//                val res = response.body()
-//                if(res != null) {
-//                    mCharacterList.addAll(res)
-//                }
-
-                Log.d("teste", mCharacterList.toString())
-                mAdapter.notifyDataSetChanged()
-
-//                Lista de ocupações
-            }
-
-            override fun onFailure(call: Call<List<CharactersModel>>, t: Throwable) {
-                val s = t.message
-            }
-        })
+        verifyCache(call)
 
         mRecyclerview = findViewById(R.id.characters_list)
         mRecyclerview.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         mRecyclerview.adapter = mAdapter
+    }
+
+    private fun verifyCache(call: Call<List<CharactersModel>>) {
+        val characters = mRepository.getAll()
+        if (characters.isEmpty()) {
+            call.enqueue(
+                object : Callback<List<CharactersModel>> {
+
+                    override fun onResponse(
+                        call: Call<List<CharactersModel>>,
+                        response: Response<List<CharactersModel>>
+                    ) {
+                        response.body()?.let { charactersList ->
+                            mCharacterList.addAll(charactersList)
+                        }
+
+                        mCharacterList.forEach { charactersModel ->
+                            mRepository.save(charactersModel)
+                        }
+
+                        Log.d("teste", mCharacterList.toString())
+                        mAdapter.notifyDataSetChanged()
+
+//                Lista de ocupações
+                    }
+
+                    override fun onFailure(call: Call<List<CharactersModel>>, t: Throwable) {
+                        val s = t.message
+                    }
+                }
+            )
+        } else {
+            mCharacterList.addAll(characters)
+            mAdapter.notifyDataSetChanged()
+        }
     }
 }
